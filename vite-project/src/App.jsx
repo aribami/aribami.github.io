39753e4265dad258584fff99e4aeb8b1592df0f2
aribami <84331768+aribami@users.noreply.github.com>
2026-05-19 15:13:22 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,5 @@
-import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 
 export default function App() {
-  return (
-    <main>
-      <Navbar />
-      <Hero />
-    </main>
-  )
+  return <Hero />
 }
