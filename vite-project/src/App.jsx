28953356c2b3039d87f340134cdb72f21998bc3c
aribami <84331768+aribami@users.noreply.github.com>
2026-05-19 14:48:35 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,36 +1,11 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-import About from "./components/About"
-import Skills from "./components/Skills"
-import Experience from "./components/Experience"
-import Projects from "./components/Projects"
-import Achievements from "./components/Achievements"
-import Certifications from "./components/Certifications"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
 
 export default function App() {
   return (
-    <div className="bg-[#020617] text-white overflow-x-hidden">
+    <main>
       <Navbar />
-
       <Hero />
-
-      <About />
-
-      <Skills />
-
-      <Experience />
-
-      <Projects />
-
-      <Achievements />
-
-      <Certifications />
-
-      <Contact />
-
-      <Footer />
-    </div>
+    </main>
   )
 }
